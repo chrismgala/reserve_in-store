4bc94922c91380647c77530888a4e08bcee1ceca
r35yan <r35yan@edu.uwaterloo.ca>
2018-06-06 10:27:29 -0400

    Log errors and changed rdoc

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,22 +1,24 @@
 module ReservationsHelper
   ##
-  # Retrieves a single Shopify product by its ID
-  # Return false if it raised an error
-  def product(id, params = {})
+  # @param id [String] ID of the product we want to lookup
+  # @return [ShopifyAPI::Product|FalseClass]  Return false if it raised an error, or the Product object if successful.
+  def product(id)
     begin
       ShopifyAPI::Product.find(id)
     rescue
+      Rails.logger.error 'Error! Retrieve Shopify product by ID failed'
       return false
     end
   end
 
   ##
-  # Retrieves a single Shopify variant by its ID
-  # Return false if it raised an error
+  # @param id [String] ID of the variant we want to lookup
+  # @return [ShopifyAPI::Variant|FalseClass]  Return false if it raised an error, or the Variant object if successful.
   def variant(id)
     begin
       ShopifyAPI::Variant.find(id)
     rescue
+      Rails.logger.error 'Error! Retrieve Shopify variant by ID failed'
       return false
     end
   end

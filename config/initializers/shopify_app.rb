d37f2e06a6a6b3d848914b5357f0be8cf2c8660e
r35yan <r35yan@edu.uwaterloo.ca>
2018-06-12 15:02:28 -0400

    changed shopify_app.rb configuration

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,8 +1,8 @@
 ShopifyApp.configure do |config|
-  config.application_name = "Reserve In-Store"
+  config.application_name = 'Reserve In-Store'
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.scope = "read_orders, read_products"
+  config.scope = 'read_products, read_themes, write_themes, read_script_tags, write_script_tags' # read_customers,write_customers
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Store

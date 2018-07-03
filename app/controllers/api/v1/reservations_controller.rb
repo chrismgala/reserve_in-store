6e0ab18ec1890de10913c6a68934b57c0604b693
RuomeiYan <34190239+RuomeiYan@users.noreply.github.com>
2018-07-03 17:28:11 -0400

    Create a new in-store reservation when the form is submitted (#4)
    Create a new in-store reservation when the form is submitted

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ReservationsController < ApiController
-      # skip_before_action :verify_authenticity_token, only: [:create]
+      protect_from_forgery with: :null_session, only: [:create]
 
       ##
       # GET /api/v1/modal
@@ -10,23 +10,23 @@ module Api
         @variant_title = params[:variant_title]
       end
 
-      # ##
-      # # POST /api/v1/store_reservations
-      # def create
-      #   @reservation = Reservation.new(reservation_params.merge(store: @store))
-      #   if @reservation.save
-      #     render json: 'test', status: :ok
-      #   else
-      #     render json: @reservation.errors, status: :unprocessable_entity
-      #   end
-      # end
-      #
-      # private
-      #
-      # def reservation_params
-      #   params.fetch(:reservation, {}).except(:location_name).permit(:customer_name, :customer_email, :customer_phone, :location_id,
-      #                                         :platform_product_id, :platform_variant_id, :comments, :fulfilled)
-      # end
+      ##
+      # POST /api/v1/store_reservations
+      def create
+        @reservation = Reservation.new(reservation_params.merge(store: @store))
+        if @reservation.save
+          render plain: @store.success_msg, status: :ok
+        else
+          render json: @reservation.errors.full_messages, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def reservation_params
+        params.fetch(:reservation, {}).permit(:customer_name, :customer_email, :customer_phone, :location_id,
+                                                                     :platform_product_id, :platform_variant_id, :comments, :fulfilled)
+      end
 
     end
   end

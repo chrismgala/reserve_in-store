110e39ee7f183f6a3c0b9e3606f1fd0b763e3f83
r35yan <r35yan@edu.uwaterloo.ca>
2018-08-20 19:18:36 -0400

    Add config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Setting this so we can catch emails in mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {address: 'localhost', port: 1025}
+  config.action_mailer.asset_host = ENV["BASE_APP_URL"]
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.

14b2db930d068cab100d1526fe886b097d357b93
r35yan <r35yan@edu.uwaterloo.ca>
2018-08-23 11:32:51 -0400

    To avoid weird redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'stores#settings'
+  get 'auth/shopify/callback' => 'callbacks#callback'
+
   resources :reservations
   resources :locations
   get 'stores/settings'
@@ -11,7 +14,6 @@ Rails.application.routes.draw do
     end
   end
 
-  root :to => 'stores#settings'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

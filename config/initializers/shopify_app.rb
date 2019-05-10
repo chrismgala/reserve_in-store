e566620bddaf470701a9a4a75c5c74fc31b034d5
Jay El-Kaake <najibkaake@gmail.com>
2019-05-10 15:33:44 -0400

    fix install error

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,7 +4,7 @@ ShopifyApp.configure do |config|
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET'].presence || ENV['SHOPIFY_API_SECRET'].presence
   config.scope = 'read_products, read_themes, write_themes, read_script_tags, write_script_tags, read_locations, read_inventory, read_locations, read_orders, write_orders, read_draft_orders, write_draft_orders, read_product_listings, read_content, write_content, read_checkouts, write_checkouts, read_analytics, read_customers, write_customers' # read_customers,write_customers
   config.embedded_app = true
-  config.after_authenticate_job = { job: "AppInstalledJob" }
+  config.after_authenticate_job = { job: AppInstalledJob }
   config.session_repository = Store
   config.webhooks = [
       {topic: 'app/uninstalled', address: "#{ENV['BASE_APP_URL']}/webhooks/app_uninstalled", format: 'json'},

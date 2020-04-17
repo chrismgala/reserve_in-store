01020c971d56a206bf6cacb3c497a6c9c3860b21
Jay El-Kaake <najibkaake@gmail.com>
2020-04-17 09:20:43 -0400

    Improve background job resilience and ignore 429 errors in webhook jobs

--- app/jobs/inventory_levels_update_job.rb
+++ app/jobs/inventory_levels_update_job.rb
@@ -1,13 +1,22 @@
 class InventoryLevelsUpdateJob < ActiveJob::Base
+  retry_on ActiveResource::ClientError
+
   def perform(shop_domain:, webhook:)
-    store = Store.find_by(shopify_domain: shop_domain)
+    @store = Store.find_by(shopify_domain: shop_domain)
 
-    return if store.blank?
+    return if @store.blank?
 
-    store.cached_api.clear_product_cache(webhook[:id])
-    store.cached_api.product(webhook[:id])
+    @store.cached_api.clear_product_cache(webhook[:id])
+    @store.cached_api.product(webhook[:id])
 
-    # TODO - for now just logging, later we will need to refresh cache
-    ForcedLogger.log("InventoryLevelsUpdateJob called.", store: store.id)
+
+  rescue ActiveResource::ClientError => e
+    if e.try(:response).try(:code).to_i == 429
+      ForcedLogger.log(e, store: @store.try(:id))
+    else
+      raise e
+    end
+    rescue ActiveResource::ResourceNotFound
+      # Product no longer exists so don't worry about updating its inventory
   end
 end
--- app/jobs/products_create_job.rb
+++ app/jobs/products_create_job.rb
@@ -1,13 +1,17 @@
 class ProductsCreateJob < ActiveJob::Base
   def perform(shop_domain:, webhook:)
-    store = Store.find_by(shopify_domain: shop_domain)
+    @store = Store.find_by(shopify_domain: shop_domain)
 
-    return if store.blank?
+    return if @store.blank?
 
-    store.cached_api.clear_product_cache(webhook[:id])
-    store.cached_api.product(webhook[:id])
+    @store.cached_api.clear_product_cache(webhook[:id])
+    @store.cached_api.product(webhook[:id])
 
-    # TODO - for now just logging, later we will need to refresh cache
-    ForcedLogger.log("ProductsCreateJob called.", store: store.id)
+  rescue ActiveResource::ClientError => e
+    if e.try(:response).try(:code).to_i == 429
+      ForcedLogger.log(e, store: @store.try(:id))
+    else
+      raise e
+    end
   end
 end

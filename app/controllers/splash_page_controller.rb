cf1af16cac5df71719bb04f159dd1650c89b5bb0
kadukia123 <adukiakopila@gmail.com>
2022-07-07 15:25:39 +0700

    clean / remove code that is not required.

--- app/controllers/splash_page_controller.rb
+++ app/controllers/splash_page_controller.rb
@@ -1,5 +1,11 @@
 class SplashPageController < ApplicationController
   include ShopifyApp::EmbeddedApp
   include ShopifyApp::RequireKnownShop
-  include ShopifyApp::ShopAccessScopesVerification
+  # include ShopifyApp::ShopAccessScopesVerification
+
+  helper_method :hide_menu?, :embedded_mode?
+
+  def hide_menu?
+    true
+  end
 end
